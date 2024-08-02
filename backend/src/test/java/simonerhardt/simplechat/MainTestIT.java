6d104ea25b6be6b0202a29646f7acb18ac55e87d
Simon Erhardt <simon.erhardt@pointsharp.com>
2024-08-02 15:37:53 +0200

    Simplified main class, added tests.

---
+++ backend/src/test/java/simonerhardt/simplechat/MainTestIT.java
@@ -0,0 +1,13 @@
+package simonerhardt.simplechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTestIT {
+
+	@Test
+	void contextLoads() {
+		// Nothing else to test here
+	}
+}
